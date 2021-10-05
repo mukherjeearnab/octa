ccebac9eca9006721192f1f9a012554d9cdcf6e8
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-10-05 21:24:39 +0530

    Updated add command

--- add.py
+++ add.py
@@ -2,6 +2,7 @@ import os
 from zipfile import ZipFile
 import hashlib
 import pandas as pd
+import time
 
 
 def run():
@@ -9,28 +10,44 @@ def run():
     files = _getAllFiles(rootdir)
     filesdf = _createFileDataFrame(files)
 
-    filesdf.to_csv(os.path.join('.', '.octa', 'files.csv'), index=False)
     print(f'Added {len(files)} files.')
 
 
+def _createCSVDump(filesdf):
+    filepath = os.path.join('.', '.octa', 'files.csv')
+    filesdf.to_csv(filepath), index = False)
+    csvHash=_getHash(filepath)
+    timestamp=_getTimestamp()
+
+    # Rename file to Stage Hash
+    os.rename(filepath, os.path.join('.', '.octa', f'{csvHash}.csv'))
+
+    # Add Stage to index file
+    with open(os.path.join('.', '.octa', 'index.txt'), 'w') as out:
+        out.write(f'{csvHash},{timestamp}\n')
+
+def _getTimestamp():
+    return time.time()
+
+
 def _createFileDataFrame(files):
-    file_hash_list = []
+    file_hash_list=[]
 
     # Generate MD5 Hashes of all files
-    index = 1
+    index=1
     for filename in files:
-        print(f"Adding ({index}/{len(files)})", end='\r')
-        file_hash = _getHash(filename)
+        print(f"Adding ({index}/{len(files)})", end = '\r')
+        file_hash=_getHash(filename)
         file_hash_list.append([file_hash, filename])
 
     # Create Dataframe form 2D List
-    file_hash_df = pd.DataFrame(file_hash_list, columns=['Hash', 'FilePath'])
+    file_hash_df=pd.DataFrame(file_hash_list, columns = ['Hash', 'FilePath'])
 
     return file_hash_df
 
 
 def _getHash(filename):
-    md5_hash = hashlib.md5()
+    md5_hash=hashlib.md5()
     with open(filename, "rb") as f:
         # Read and update hash in chunks of 4K
         for byte_block in iter(lambda: f.read(4096), b""):
@@ -39,7 +56,7 @@ def _getHash(filename):
 
 
 def _getAllFiles(path):
-    files = _absoluteFilePaths(path)
+    files= _absoluteFilePaths(path)
     return list(files)
 
 
