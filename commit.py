331c9e56a137d5975c9b1f5c7530edf9b5244168
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-10-05 20:59:36 +0530

    Init 'commit' command

---
+++ commit.py
@@ -0,0 +1,60 @@
+import os
+from zipfile import ZipFile
+import hashlib
+import pandas as pd
+
+
+def run():
+    hashfile = os.path.join('.', '.octa', 'files.csv')
+    # Check if files.csv exists
+    if not os.path.isfile(hashfile):
+        print("Error! Files are not staged for commit.\nUse octa add to stage files for commit.")
+        return
+
+    # Check for unstaged and modified files
+    print("Checking for any unstaged changes.")
+    files = _getAllFiles('.')
+    filesdf = _createFileDataFrame(files)
+    prexdf = pd.read_csv(hashfile)
+    if not filesdf.equals(prexdf):
+        print("Error! Untracked or modified files present.\nUse octa add to unstaged and modified files for commit.")
+        return
+
+
+def _createFileDataFrame(files):
+    file_hash_list = []
+
+    # Generate MD5 Hashes of all files
+    index = 1
+    for filename in files:
+        print(f"Calculating file ({index}/{len(files)})", end='\r')
+        file_hash = _getHash(filename)
+        file_hash_list.append([file_hash, filename])
+
+    # Create Dataframe form 2D List
+    file_hash_df = pd.DataFrame(file_hash_list, columns=['Hash', 'FilePath'])
+
+    return file_hash_df
+
+
+def _getHash(filename):
+    md5_hash = hashlib.md5()
+    with open(filename, "rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+        return md5_hash.hexdigest()
+
+
+def _getAllFiles(path):
+    files = _absoluteFilePaths(path)
+    return list(files)
+
+
+def _absoluteFilePaths(directory):
+    for dirpath, dirnames, filenames in os.walk(directory):
+        if ".octa" in dirnames:
+            dirnames.remove(".octa")
+
+        for f in filenames:
+            yield os.path.relpath(os.path.join(dirpath, f)).replace(os.sep, '/')
--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import auxiliary
 
 import init
 import add
+import commit
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'init':
         init.run()
     elif sys.argv[1] == 'add':
         add.run()
+    elif sys.argv[1] == 'commit':
+        commit.run()
     else:
         print("Invalid arguments:", sys.argv[1])
         auxiliary.showHelp()
