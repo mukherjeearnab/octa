ff5c52ccac5e1f6036553e0438b21ad16dee7ccc
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-10-05 20:03:23 +0530

    Added 'add' command

---
+++ add.py
@@ -0,0 +1,49 @@
+import os
+from zipfile import ZipFile
+import hashlib
+import pandas as pd
+
+
+def run():
+    rootdir = './'
+    files = _getAllFiles(rootdir)
+    filesdf = _createFileDataFrame(files)
+
+    filesdf.to_csv(os.path.join('.', '.octa', 'files.csv'), index=False)
+    print(f'Added {len(files)} files.')
+
+
+def _createFileDataFrame(files):
+    file_hash_list = []
+
+    # Generate MD5 Hashes of all files
+    index = 1
+    for filename in files:
+        print(f"Adding ({index}/{len(files)})", end='\r')
+        file_hash = _getHash(filename)
+        file_hash_list.append([file_hash, filename])
+
+    # Create Dataframe form 2D List
+    file_hash_df = pd.DataFrame(file_hash_list, columns=['Hash', 'FilePath'])
+
+    return file_hash_df
+
+
+def _getHash(filename):
+    md5_hash = hashlib.md5()
+    with open(filename, "rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+        return md5_hash.hexdigest()
+
+
+def _getAllFiles(path):
+    files = _absoluteFilePaths(path)
+    return list(files)
+
+
+def _absoluteFilePaths(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.relpath(os.path.join(dirpath, f)).replace(os.sep, '/')
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import strings
 import auxiliary
 
 import init
+import add
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'init':
         init.run()
+    elif sys.argv[1] == 'add':
+        add.run()
     else:
         print("Invalid arguments:", sys.argv[1])
         auxiliary.showHelp()
