277ac7b3fccd3d9763128bf6aadf1d6816a67efd
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-10-05 20:11:21 +0530

    Updated 'init', added index file

--- init.py
+++ init.py
@@ -2,10 +2,23 @@ import os
 
 
 def run():
+    repoFlag = _createOctaRepository()
+    if repoFlag:
+        _createIndexingFile()
 
+
+def _createIndexingFile():
+    with open(os.path.join('.', '.octa', 'index.txt'), 'w') as out:
+        out.write("")
+    print("Initialized index file.")
+
+
+def _createOctaRepository():
     # Create .octa folder in root directory
     if not os.path.exists('.octa'):
         print("Initialized empty octa repository.")
         os.makedirs('.octa')
+        return True
     else:
         print("Octa repository already exists.")
+        return False
